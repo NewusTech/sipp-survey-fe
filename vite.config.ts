23a599f3fb8aa7e86f68521838a8bd6dd425feed
Aldi Nugraha <aldi.nugrahatk@gmail.com>
2024-06-03 11:38:44 +0700

    feat: implementation PWA

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,56 @@
-import path from "path"
-import react from "@vitejs/plugin-react"
-import { defineConfig } from "vite"
+import path from "path";
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
+import { VitePWA } from "vite-plugin-pwa";
+import type { VitePWAOptions } from "vite-plugin-pwa";
+
+const manifestForPlugIn: Partial<VitePWAOptions> = {
+  registerType: "prompt",
+  manifest: {
+    name: "SIPPP PUPR TULANG BAWANG BARAT",
+    short_name: "SIPPP Apps",
+    description:
+      "Sistem Informasi Pelaksanaan, Pengawasan, Pelaporan Tulang Bawang Barat",
+    icons: [
+      {
+        src: "/android-chrome-192x192.png",
+        sizes: "192x192",
+        type: "image/png",
+        purpose: "favicon",
+      },
+      {
+        src: "/android-chrome-512x512.png",
+        sizes: "512x512",
+        type: "image/png",
+        purpose: "favicon",
+      },
+      {
+        src: "/apple-touch-icon.png",
+        sizes: "180x180",
+        type: "image/png",
+        purpose: "apple touch icon",
+      },
+      {
+        src: "/maskable_icon.png",
+        sizes: "512x512",
+        type: "image/png",
+        purpose: "any maskable",
+      },
+    ],
+    theme_color: "#171717",
+    background_color: "#f0e7db",
+    display: "standalone",
+    scope: "/",
+    start_url: "/",
+    orientation: "portrait",
+  },
+};
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), VitePWA(manifestForPlugIn)],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
-})
+});
